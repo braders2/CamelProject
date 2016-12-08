8b851b13095fc71bc29ae322528332881d0660ff
Mateusz Dobrowolski <mateusz.dobrowolski@britenet.com.pl>
2016-12-08 11:17:41 +0100

    added missing files

--- src/main/java/com/camel/processor/user/GetUserProcessor.java
+++ src/main/java/com/camel/processor/user/GetUserProcessor.java
@@ -4,19 +4,20 @@ import com.camel.dao.UserRepository;
 import com.camel.dao.impl.UserRepositoryImpl;
 import com.camel.dto.UserDTO;
 import com.camel.tables.tables.records.UserRecord;
+import com.camel.transform.GenericTransformer;
 import com.camel.transform.impl.UserTransformerImpl;
 import com.camel.utils.Precondition;
 import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
-import org.apache.camel.component.restlet.RestletConstants;
 import org.restlet.Response;
-import org.restlet.data.Status;
 
 import java.util.Optional;
 
 import static com.camel.utils.Const.HEADER_ELEMENT_ID;
+import static org.apache.camel.component.restlet.RestletConstants.RESTLET_RESPONSE;
+import static org.restlet.data.Status.SUCCESS_NO_CONTENT;
 
 public class GetUserProcessor implements Processor {
 
@@ -31,13 +32,13 @@ public class GetUserProcessor implements Processor {
         Optional<UserRecord> userRecord = userRepository.get(Long.parseLong(userId));
         if (userRecord.isPresent()) {
             UserRecord userData = userRecord.get();
-            UserTransformerImpl userTransformer = new UserTransformerImpl();
+            GenericTransformer<UserDTO, UserRecord> userTransformer = new UserTransformerImpl();
             UserDTO userDTO = userTransformer.convertToDto(userData);
 
             exchange.getIn().setBody(gson.toJson(userDTO));
         } else {
-            Response response = exchange.getIn().getHeader(RestletConstants.RESTLET_RESPONSE, Response.class);
-            response.setStatus(Status.SUCCESS_NO_CONTENT);
+            Response response = exchange.getIn().getHeader(RESTLET_RESPONSE, Response.class);
+            response.setStatus(SUCCESS_NO_CONTENT);
             exchange.getOut().setBody(response);
         }
     }
